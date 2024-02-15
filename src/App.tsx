45904c598dc177e7ebbff92dcc766f012a6d0647
nmobsis <nmobsis@gmail.com>
2024-02-15 21:41:05 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
+      "Hindi",
+      "sure kah nah gd?",
       "What if I asked really nicely?",
       "Pretty please",
       "With a chocolate rice cake on top",
@@ -26,7 +26,7 @@ export default function Page() {
       "please babe",
       ":((((",
       "PRETTY PLEASE",
-      "Estoy muerto",
+      "No No No No No",
       "No :(",
     ];
 
@@ -38,7 +38,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!!</div>
         </>
       ) : (
         <>
